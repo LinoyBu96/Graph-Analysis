d933538c64111039dc6c45011748324500fed2ae
LinoyBu96 <linoy.bsr@gmail.com>
2024-06-01 11:56:33 +0300

    Update arguments

--- src/main.py
+++ src/main.py
@@ -1,35 +1,44 @@
 import argparse
 from pyspark.sql import SparkSession
-from utils import load_csv_to_df, find_common_neighbors
+from utils import load_csv_to_df, find_common_neighbors, handle_output
+import sys
 import os
 
-JAVA_HOME = "C:\Program Files\Java\jdk-22"
-
-os.environ["JAVA_HOME"] = JAVA_HOME
 
 def parse_args() -> None:
     parser = argparse.ArgumentParser(description='Graph Analysis to find common neighbors.')
-    parser.add_argument('-n', type=int, default=3, help='Number of top node pairs to retrieve')
-    parser.add_argument('--input', help='Input path to the CSV files containing the graph data')
-    parser.add_argument('--output', choices=['show', 'save'], help='Output mode: "show" on console or "save" to CSV file')
+    parser.add_argument('-n', type=int, required=True, help='Number of top node pairs to retrieve')  # Require the number of node pairs
+    parser.add_argument('--input', required=True, help='Input path to the CSV files containing the graph data')  # Require input path
+    parser.add_argument('--output', choices=['show', 'save'], required=True, help='Output mode: "show" on console or "save" to CSV file')  # Require output mode
     parser.add_argument('--output_path', help='Path to save the results if output mode is "save"')
-    return parser.parse_args()
+    args = parser.parse_args()
 
-def main(session_name: str="Graph Analysis") -> None:
-    args = parse_args()
+    if args.output == 'save' and not args.output_path:
+        parser.error("--output 'save' requires --output_path to be specified.")
+
+    return args
+
+def validate_output_path(path):
+    if not os.path.exists(os.path.dirname(path)):
+        raise ValueError(f"Output directory does not exist: {os.path.dirname(path)}")
+
+def run_analysis(n, input_path, output_mode, output_path=None):
+    # TODO: Try with hadoop
     spark = SparkSession.builder \
-        .appName(session_name) \
+        .appName("Graph Analysis") \
         .getOrCreate()
 
-    df = load_csv_to_df(spark, args.input)
-    top_pairs = find_common_neighbors(df, args.n)
+    df = load_csv_to_df(spark, input_path)
+    top_pairs = find_common_neighbors(df, n)  #TODO: Duplicated edge
+    handle_output(top_pairs, output_mode, output_path)
 
-    if args.output == 'show':
-        top_pairs.show()
-    elif args.output == 'save':
-        top_pairs.write.csv(args.output_path, header=True, mode='overwrite')  # TODO: mode
-    spark.stop()
+def main() -> None:
+    args = parse_args()
+    if args.output == 'save' and args.output_path:
+        validate_output_path(args.output_path)
+    run_analysis(args.n, args.input, args.output, args.output_path)
 
 if __name__ == "__main__":
+    print("start")
     main()
     # TODO: Add prints for logs?
--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,14 @@
-import os
 from pyspark.sql.functions import col, DataFrame
+import yaml
+
+def load_config(file_path):
+    with open(file_path, 'r') as file:
+        config = yaml.safe_load(file)
+    return config
+
 
 def load_csv_to_df(spark_session, path: str) -> DataFrame:
-    # Read CSV files into DataFrame
+    # TODO: define specific schema?
     return spark_session.read.csv(path, header=True, inferSchema=True)
 
 def find_common_neighbors(df, n):
@@ -11,9 +17,17 @@ def find_common_neighbors(df, n):
                 (col('g1.dst') == col('g2.dst')) & (col('g1.src') < col('g2.src'))) \
         .select(
             col('g1.src').alias('node1'),
-            col('g2.src').alias('node2')
+            col('g2.src').alias('node2')    
     )
     common_neighbors_df = pairs_df.groupBy('node1', 'node2').count() \
         .withColumnRenamed('count', 'common')
     return common_neighbors_df.orderBy(col('common').desc()).limit(n)
     
+def handle_output(df, output_mode, output_path=None):
+    if output_mode == 'show':
+        df.show()
+    elif output_mode == 'save':
+        if output_path is not None:
+            df.write.csv(output_path, header=True, mode='overwrite')
+        else:
+            raise ValueError("Output path must be provided for save mode.")
